c268110ba28c4bf49674d263acb44b9861d5168d
Worood Abdou <woroodabdou@gmail.com>
2026-08-31 09:24:02 +0300

    Initial Spring Boot project scaffold

---
+++ src/main/java/com/attest/attest/AttestApplication.java
@@ -0,0 +1,13 @@
+package com.attest.attest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attest/attest/AttestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attest.attest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
